62bb2927c12f375d4b28693bdc2dd82ef5af85f9
Manmay Ghosh <89407915+ManmayGhosh@users.noreply.github.com>
2026-02-24 11:28:37 +0530

    Time: 0 ms (100%), Space: 58.4 MB (56.52%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.ts
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.ts
@@ -24,14 +24,3 @@ function DFS(rt : TreeNode, x : number) : number{
         return x;
     return DFS(rt.left, x) + DFS(rt.right, x);
 }
-// int sumRootToLeaf(TreeNode* root) {
-//         return DFS(root, 0);
-//     }
-//     int DFS(TreeNode* rt, int x) {
-//         if (rt == nullptr)
-//             return 0;
-//         x = x * 2 + rt->val;
-//         if (rt->left == rt->right)
-//             return x;
-//         return DFS(rt->left, x) + DFS(rt->right, x);
-//     }
