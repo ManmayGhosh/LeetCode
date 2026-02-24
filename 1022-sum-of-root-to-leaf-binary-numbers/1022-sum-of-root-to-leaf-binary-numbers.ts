b2d7c854bb31726f7c2985b99a868a3561d17b91
Manmay Ghosh <89407915+ManmayGhosh@users.noreply.github.com>
2026-02-24 11:28:17 +0530

    Time: 0 ms (100%), Space: 58 MB (82.61%) - LeetHub

---
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sumRootToLeaf(root: TreeNode | null): number {
+    return DFS(root, 0);
+};
+
+function DFS(rt : TreeNode, x : number) : number{
+    if (rt == null)
+        return 0;
+    x = x * 2 + rt.val;
+    if (rt.left == rt.right)
+        return x;
+    return DFS(rt.left, x) + DFS(rt.right, x);
+}
+// int sumRootToLeaf(TreeNode* root) {
+//         return DFS(root, 0);
+//     }
+//     int DFS(TreeNode* rt, int x) {
+//         if (rt == nullptr)
+//             return 0;
+//         x = x * 2 + rt->val;
+//         if (rt->left == rt->right)
+//             return x;
+//         return DFS(rt->left, x) + DFS(rt->right, x);
+//     }
